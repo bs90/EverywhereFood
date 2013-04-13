c2e6ccff879d993bb4d2bad57d2e36096e874b92
Tran Ba Trong <bs90_htdd@yahoo.com.vn>
2013-04-13 09:28:53 +0700

    Revert "Revert "Revert "undone"""
    This reverts commit 539d4e45d2bf315e4924766547f06f5668166fbc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Everywherefood::Application.routes.draw do
+  get "static_pages/about"
+  match "/about", to:"static_pages#about"
+  
+  get "static_pages/help"
+
   get "admin/index"
 
   get "sessions/create"
